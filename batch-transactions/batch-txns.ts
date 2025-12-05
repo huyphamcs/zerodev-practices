35c94eb76a9f7984f4d1269e1c0edd23a31c940e
huyphamcs <huy.phamcs@hcmut.edu.vn>
2025-12-05 11:50:06 +0700

    Add utils and batch-transactions folder

---
+++ batch-transactions/batch-txns.ts
@@ -0,0 +1,26 @@
+
+import { KERNEL_V3_1 } from "@zerodev/sdk/constants"
+import {getKernelClient} from "../utils"
+import { zeroAddress } from "viem";
+
+
+const main = async () => {
+    const kernelClient = await getKernelClient("0.7", KERNEL_V3_1);
+
+    const txHash = await kernelClient.sendTransaction({
+        calls:[
+            {
+                to: zeroAddress,
+                value: BigInt(0),
+                data: "0x"
+            },
+            {
+                to: zeroAddress,
+                value: BigInt(0),
+                data: "0x"
+            }
+        ]
+    })
+    console.log(txHash);
+}
+main()
---
+++ utils.ts
@@ -0,0 +1,58 @@
+import { KERNEL_V3_1, getEntryPoint } from "@zerodev/sdk/constants";
+import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
+import { sepolia } from "viem/chains";
+import "dotenv/config";
+import { EntryPointVersion } from "viem/account-abstraction";
+import { GetKernelVersion } from "@zerodev/sdk/types/kernel";
+import { signerToEcdsaValidator } from "@zerodev/ecdsa-validator";
+import { createPublicClient, http } from "viem";
+import { createKernelAccount, createKernelAccountClient, createZeroDevPaymasterClient } from "@zerodev/sdk";
+
+
+const ZERODEV_RPC=process.env.ZERODEV_RPC
+const chain = sepolia
+const kernelVersion = KERNEL_V3_1
+const privateKey = generatePrivateKey();
+const signer = privateKeyToAccount(privateKey);
+const publicClient = createPublicClient({
+    chain: chain,
+    transport: http(ZERODEV_RPC)
+})
+
+export const getKernelClient = async <entryPointVersion extends EntryPointVersion> (
+    entryPointVersion_: entryPointVersion,
+    kernelVersion: GetKernelVersion<entryPointVersion>
+) => {
+
+    const entryPoint = getEntryPoint(entryPointVersion_)
+    const ecdsaValidator = await signerToEcdsaValidator(publicClient, {
+        signer,
+        entryPoint,
+        kernelVersion
+    })
+    const account = await createKernelAccount(publicClient, {
+        plugins:{
+            sudo: ecdsaValidator,
+        },
+        entryPoint,
+        kernelVersion
+    })
+    console.log("Account address: ", account.address);
+
+    const paymasterClient = createZeroDevPaymasterClient({
+        chain,
+        transport: http(ZERODEV_RPC)
+    })
+    
+    const kernelClient = await createKernelAccountClient({
+        account,
+        chain,
+        bundlerTransport: http(ZERODEV_RPC),
+        client: publicClient,
+        paymaster:
+        {
+            getPaymasterData: (userOperation) => {return paymasterClient.sponsorUserOperation({userOperation})}
+        }
+    })
+    return kernelClient
+}
